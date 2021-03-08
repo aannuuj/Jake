63939fd05351aa00083451411ec1d45a9557b5ae
aannuuj <hariompalkar@gmail.com>
2021-03-08 17:12:47 +0530

    feat: Image View

--- Jake/ViewController.swift
+++ Jake/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    fileprivate let imageView = UIImageView(image: UIImage(named: "jake"))
+    fileprivate let cardHeight = UIScreen.main.bounds.height / 2.2
+    fileprivate let cardWidth = UIScreen.main.bounds.width - ( UIScreen.main.bounds.width / 5 )
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // backgrround color
+        view.backgroundColor = UIColor.white
+        
+        // UIimage view
+        view.addSubview(imageView)
+        imageView.contentMode = .scaleAspectFill
+        
+        // auto layout constraints
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: cardHeight).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: cardWidth ).isActive = true
+       
+        // corner radius
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 20
+        
     }
-
-
 }
 
