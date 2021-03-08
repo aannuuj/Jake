cccae16d17b286d19fdf6e0857952265d559b07f
aannuuj <hariompalkar@gmail.com>
2021-03-09 01:25:14 +0530

    feat: Pan Gesture

--- Jake/ViewController.swift
+++ Jake/ViewController.swift
@@ -29,11 +29,33 @@ class ViewController: UIViewController {
         imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         imageView.heightAnchor.constraint(equalToConstant: cardHeight).isActive = true
         imageView.widthAnchor.constraint(equalToConstant: cardWidth ).isActive = true
-       
+        
         // corner radius
         imageView.clipsToBounds = true
         imageView.layer.cornerRadius = 20
         
+        
+        // gesture recoginizer
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(self.handlePanGesture)))
+        
+    }
+    // obj c function for pan gesture
+    
+    @objc func handlePanGesture(gesture: UIPanGestureRecognizer) {
+        if gesture.state == .began {
+            print("Interaction started")
+        }
+        else if gesture.state == .changed {
+            // when chnaged transforming it acc to the translation
+            let translation = gesture.translation(in: self.view)
+            imageView.transform = CGAffineTransform(translationX: translation.x, y:  translation.y)
+        }
+        else if gesture.state == .ended {
+            // animating it back to initial value
+            UIView.animate(withDuration: 0.8, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 1, options: .curveLinear, animations: {self.imageView.transform = .identity})
+            print("Interaction ended")
+        }
     }
 }
 
